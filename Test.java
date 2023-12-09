56b4b72f35cc8ebe80a58ae4ca3d8f3d64bf9a9b
Bhavana-K-G <bhavanagowda768@gmail.com>
2023-12-09 19:45:07 +0530

    java

---
+++ Test.java
@@ -0,0 +1,12 @@
+class Test{
+    Test(){
+        System.out.println("gm");
+    }
+    public void get_details(){
+        System.out.println("gm");
+    }
+    public static void main(String[] args){
+        new Test();
+        new Test(); 
+    }
+}
