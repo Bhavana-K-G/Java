41fb94173631f395736793ce8bd4a12d2059b480
Bhavana-K-G <bhavanagowda768@gmail.com>
2023-12-11 20:00:54 +0530

    java

---
+++ part-1/basics/two.java
@@ -0,0 +1,8 @@
+public class two {
+    public static void main(String[] args) {
+        totalmarks (new int[]{45,56,87,98});
+    }
+    public static void totalmarks(int[] marks){
+        System.out.println(marks[0]);  
+    }
+}
