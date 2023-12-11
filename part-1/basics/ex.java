ecc42f3ade42c1654ca468f963b4c532ad352fd8
Bhavana-K-G <bhavanagowda768@gmail.com>
2023-12-11 19:49:04 +0530

    java

---
+++ part-1/basics/ex.java
@@ -0,0 +1,13 @@
+class ex {
+
+    int eid;
+    string ename;
+    double esal;
+    static string arg_name;
+    public static void main(String[] args) {
+        ex e1=new ex();
+        ex e2=new ex();
+        ex e3=new ex();
+    }
+    
+}
---
+++ part-1/basics/example.java
@@ -0,0 +1,14 @@
+class account {
+    int acc_id;
+    str acc_name;
+    account(int id, string name){
+        this.acc_id=id;
+        this.acc_name=name;
+    }
+    public static void main(str[] args) {
+        account a1=new account(101, "rahul");
+        account a2=new account(102,"sonia");
+        System.out.println(a1.acc_id+":"+a1.acc_name);
+        System.out.println(a2.acc_id+":"+a2.acc_name);
+    }
+}
