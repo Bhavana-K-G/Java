e06b5a01d9c832d68e085d3e6ff218c04c17ca55
Bhavana-K-G <bhavanagowda768@gmail.com>
2023-12-12 21:32:11 +0530

    java

---
+++ part-1/Java Basics/DataTypes/Test.java
@@ -0,0 +1,11 @@
+class Test{
+    
+public static void main(String[] args){
+	char avail='Y';
+	String	 ename="Rahul Gandhi";
+	boolean discount = true;
+	System.out.println(avail);
+	System.out.println(ename);
+	System.out.println(discount);
+	}
+}
